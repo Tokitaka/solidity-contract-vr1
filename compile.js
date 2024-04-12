7ba47c77c21c9ed022816c2c8f058eda6c435021
Juhye O <artigen3@gmail.com>
2024-04-12 13:34:01 +0900

    Compiling source code

--- compile.js
+++ compile.js
@@ -1,2 +1,15 @@
 // compile code will go here
-// soley to compile our code 
+
+// path module : generate path to Inbox.sol
+const path = require('path');
+// fs module : read contents of files
+const fs = require('fs');
+// solc : solidity compiler
+const solc = require('solc');
+
+// 1. generate path
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+// 2. read source code 
+const source = fs.readFileSync(inboxPath, 'utf8');
+// 3. compile source code / numbers of contracts
+solc.compile(source, 1);
